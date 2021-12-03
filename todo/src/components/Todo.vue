28518e806043c9152ccd794ba036a3fab7493c4c
= <=>
2021-12-03 14:32:02 +0900

    todo 앱 프로젝트 추가

---
+++ todo/src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button class="list-group-item text-left" @click="clickButton">
+            {{ label }}
+        </button>
+    </div>
+</template>
+<script>
+export default {
+    props:['label'],
+    methods:{
+        clickButton(){
+            this.$emit('componentClick');
+        }
+    }
+}
+</script>
+<style>
+    
+</style>

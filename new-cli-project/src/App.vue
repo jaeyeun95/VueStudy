c519a5b055d34032ce299423962a7e7af2a846f8
jaeyen95 <jaeyeun95@gmail.com>
2021-12-03 15:13:17 +0900

    vuetify 적용한 vue-project 추가

---
+++ new-cli-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ count }}</p>
+    <button @click="count++">추가</button>
+  </div>
+</template>
+
+<script>
+
+export default {  // es6 문법
+  name: 'App',
+
+  data () {
+    return{
+      title:'안녕하세요',
+      count: 1,
+    }
+    //
+  },
+};
+</script>

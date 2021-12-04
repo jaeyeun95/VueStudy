3b7dc56f2f2085d03abfb93c6eafd70a8f68be30
jaeyeun <jaeyeun95@gmail.com>
2021-12-04 20:15:28 +0900

    vue.js props 와 emit

---
+++ new-cli-project2/src/components/User.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="blue lighten-3 pa-3">
+    <h1>User 컴포넌트</h1>
+    <p>이름: {{ name }}</p>
+    <v-btn @click="changeName()">이름 변경</v-btn>
+    <hr />
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <UserDetail
+          :name="name"
+          :address="address"
+          :phone="phone"
+          :hasDog="hasDog"
+        >
+        </UserDetail>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <UserEdit
+          :name="name"
+          :address="address"
+          :phone="phone"
+          :hasDog="hasDog"
+          @child="parents"
+        ></UserEdit>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  components: {
+    UserDetail,
+    UserEdit,
+  },
+  data() {
+    return {
+      name: "Hoza",
+      address: "Seoul",
+      phone: "1234-5678",
+      hasDog: true,
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "Hoza";
+    },
+    parents(data) {
+      this.name = data.name;
+      this.address = data.address;
+      this.phone = data.phone;
+      this.hasDog = data.hasDog;
+      console.log("부모가 받았어!", data);
+    },
+  },
+};
+</script>
---
+++ new-cli-project2/src/components/UserDetail.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="red lighten-3 pa-3">
+    <h3>자세한 회원 정보를 확인합니다.</h3>
+    <!-- <p>{{ nameOfChild }}</p>
+    <p>{{ sayHello }}</p>
+    <v-btn @click="switchName()"> 이름 변경 </v-btn> -->
+    <v-list dense>
+      <v-list-tile>
+        <v-list-tile-content>이름:</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ name }}
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>주소:</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ address }}
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>전화번호:</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ phone }}
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>반려견유무:</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ hasDogKr }}
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </div>
+</template>
+<script>
+export default {
+  props: ["name", "address", "phone", "hasDog"],
+  // props: {
+  //   name: {
+  //     type: String, // props 받은 이름 : 데이터 형식
+  //     required: false, // 필요한지 안한지
+  //     default: "Lego", // default 값
+  //   },
+  // },
+  data() {
+    return {
+      age: 30,
+    };
+  },
+  computed: {
+    sayHello() {
+      return this.nameOfChild + " 안녕하세요";
+    },
+    hasDogKr() {
+      return this.hasDog === true ? "있음" : "없음";
+    },
+  },
+  methods: {
+    switchName() {
+      this.nameOfChild = "컴퓨터";
+    },
+  },
+};
+</script>

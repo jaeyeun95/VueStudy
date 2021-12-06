85ae4788d9a77c432043076d0459861993caedf6
jaeyen95 <jaeyeun95@gmail.com>
2021-12-06 13:01:33 +0900

    페이지네이션 추가

---
+++ bootstrapvue/src/components/pagination/Pagination.vue
@@ -0,0 +1,8 @@
+<template>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
+</template>
--- bootstrapvue/src/views/Home.vue
+++ bootstrapvue/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <h1>LoginForm</h1>
     <LoginPage></LoginPage>
+    <Pagination/>
   </div>
 </template>
 
 <script>
 import LoginPage from '@/components/Login.vue';
+import Pagination from '@/components/pagination/Pagination.vue';
 // @ is an alias to /src
 
 export default {
   name: "Home",
   components: {
     LoginPage,
+    Pagination,
   },
 };
 </script>

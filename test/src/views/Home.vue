544fddeeb12e23bd0ad6cf1571e921ab0a5ba937
jaeyen95 <jaeyeun95@gmail.com>
2021-12-22 14:35:17 +0900

    한시간만에 끝내는 Vue.js 입문 라이프사이클, 데이터 바인딩, watch 등 기본적인 문법

---
+++ test/src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>Welcome to Home! {{ title }}</h1>
+    <input type="text" value="abc" v-model="input1">
+    <button @click="getData">Get</button>
+    <button @click="setData">Set</button>
+
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <option v-for="(d,i) in options" :key="i" :value="d.v">{{ d.t }} </option>
+    </select>
+
+    <table class="table table-bordered" v-if="tableShow">
+      <tr v-for="(d,i) in options" :key="i">
+        <td>{{ d.v }}</td>
+        <td>{{ d.t }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      title: "개발자의 품격",
+      input1:"abcd",
+      options: [
+        {v:"S", t:"Seoul"},
+        {v:"J", t:"Jeju"},
+        {v:"B", t:"Busan"},
+      ],
+      region:"J",
+      tableShow: false,
+    } 
+  },
+  watch : {
+    input1(){
+      console.log(this.input1);
+    }
+  },
+  methods: {
+    getData() {
+      alert(this.input1);
+    },
+    setData(){
+      this.input1 = '12345';
+    },
+    changeRegion(){
+      alert(this.region);
+    }
+  },
+  beforeCreate() {
+    
+  },
+}
+</script>
